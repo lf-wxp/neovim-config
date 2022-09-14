c79ec292e078507efed6084cc6c6eba29f10319e
franciscowxp <franciscowxp@gmail.com>
2022-09-14 19:44:21 +0800

    feat: use new syntax to disabled lsp default format

--- lua/lsp/config/json.lua
+++ lua/lsp/config/json.lua
@@ -12,8 +12,10 @@ return {
       },
       on_attach = function(client, _)
         -- 禁用格式化功能，交给专门插件插件处理
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+        -- client.resolved_capabilities.document_formatting = false
+        -- client.resolved_capabilities.document_range_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
       end,
     })
   end,
