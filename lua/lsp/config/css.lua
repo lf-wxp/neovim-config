4520782297f56267b117856676d87ab0db5875d0
franciscowxp <franciscowxp@gmail.com>
2022-10-15 14:08:45 +0800

    update deprecated function

--- lua/lsp/config/css.lua
+++ lua/lsp/config/css.lua
@@ -2,7 +2,7 @@ local navic = require("nvim-navic")
 return {
   on_setup = function(server, bufnr)
     server.setup({
-      capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
       settings = {
         css = {
           validate = true,
