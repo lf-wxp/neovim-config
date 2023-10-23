7fa04ea9907f99751f9e6af933e93ced3961beb4
franciscowxp <franciscowxp@gmail.com>
2023-10-23 21:15:16 +0800

    feat: add new plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -367,17 +367,29 @@ require("lazy").setup({
       require('bamboo').load()
     end,
   },
-  -- {
-  --   "tamton-aquib/zone.nvim",
-  --   config = function()
-  --     require("plugin-config.zone")
-  --   end,
-  -- },
+  {
+    "tamton-aquib/zone.nvim",
+    config = function()
+      require("plugin-config.zone")
+    end,
+  },
   -- {
   --   "giusgad/pets.nvim",
   --   dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
   --   config = function()
   --     require("pets").setup()
   --   end,
-  -- }
+  -- },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  }
 })
